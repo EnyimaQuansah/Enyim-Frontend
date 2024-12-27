24543dabd58bbacbd2b032f540fb5e852db6376c
Ama Osei <amaosei1234@gmail.com>
2024-12-27 15:21:38 +0000

    Revert "finished frontend design"
    This reverts commit 273fa32c5706dfaec8f02d763f06ae635b95b326.

--- middleware/cartCount.js
+++
@@ -1,8 +0,0 @@
-const cartCountMiddleware = (req, res, next) => {
-    // Calculate cart count from session
-    res.locals.cartCount = req.session.cart ? req.session.cart.length : 0;
-    next(); // Call the next middleware or route handler
-  };
-  
-  module.exports = cartCountMiddleware;
-  
